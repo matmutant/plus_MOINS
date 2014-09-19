c9a5e5bd1b981731098ee8891f030bfe871f3be9
matmutant <matmutant@gmail.com>
2014-09-19 19:40:16 +0200

    added roud count game

--- main.c
+++ main.c
@@ -4,13 +4,14 @@
 
 int main ( int argc, char** argv )
 {
-    int nombreMystere = 0, nombreEntre = 0;
+    int mysteryNumber = 0, triedNumber = 0;
     const int MAX = 100, MIN = 1;
+    int tries = 0;
 
     // Génération du nombre aléatoire
 
     srand(time(NULL));
-    nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+    mysteryNumber = (rand() % (MAX - MIN + 1)) + MIN;
 
     /* La boucle du programme. Elle se répète tant que l'utilisateur n'a pas trouvé le nombre mystère */
 
@@ -18,16 +19,23 @@ int main ( int argc, char** argv )
     {
         // On demande le nombre
         printf("Quel est le nombre ? ");
-        scanf("%d", &nombreEntre);
+        scanf("%d", &triedNumber);
+        // increment tries number
+                                tries++;
 
         // On compare le nombre entré avec le nombre mystère
 
-        if (nombreMystere > nombreEntre)
+        if (mysteryNumber > triedNumber)
             printf("C'est plus !\n\n");
-        else if (nombreMystere < nombreEntre)
+        else if (mysteryNumber < triedNumber)
             printf("C'est moins !\n\n");
         else
-            printf ("Bravo, vous avez trouve le nombre mystere !!!\n\n");
-    } while (nombreEntre != nombreMystere);
+            {
+            printf ("Well done, you found the Mystery Number ");
+            printf("in %d tries !!!\n\n", tries);
+            }
+
+
+    } while (triedNumber != mysteryNumber);
 return 0;
 }
