1daf35b2cff6902751b52d84f41741a977a1144d
matmutant <matmutant@gmail.com>
2014-09-19 20:52:43 +0200

    added game menu (4. Custom needs to be redone)

--- main.c
+++ main.c
@@ -2,29 +2,19 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main ( int argc, char** argv )
+int game(int max, int min)
 {
+            printf("values between : %d and %d\n\n", min, max);
     int mysteryNumber = 0, triedNumber = 0;
-    int MAX = 100, MIN = 1;
     int tries = 0;
-    int playAgain = 1;
+    //int playAgain = 1;
 
-    /*
-    Calling time as random generator
-    Utilisation du moment comme générateur d'aléatoire
-    */
-    srand(time(NULL));
-    /*
-    First loop running until gamer is fed up with playing
-    Première boucle qui tourne jusqu'à ce que le joueur veuille s'arreter
-    */
-while (playAgain !=0)
-{
+
     /*
     Creating random number
     Céation du nombre aléatoire
     */
-    mysteryNumber = (rand() % (MAX - MIN + 1)) + MIN;
+    mysteryNumber = (rand() % (max - min + 1)) + min;
 
     /*
     Inner loop asking for number untill gamer find the right one.
@@ -56,8 +46,8 @@ while (playAgain !=0)
             {
             printf ("Well done, you found the Mystery Number ");
             printf("in %d tries !!!\n\n", tries);
-            printf("Wanna play Again? (1: yes; 0:no)");
-            scanf("%d", &playAgain);
+            //printf("Wanna play Again? (1: yes; 0:no)");
+            //scanf("%d", &playAgain);
             }
         else
         {
@@ -66,6 +56,68 @@ while (playAgain !=0)
         }
 
     } while (triedNumber != mysteryNumber);
-}
 return 0;
+}
+
+
+
+
+
+int main ( int argc, char** argv )
+{
+    /*
+    Calling time as random generator
+    Utilisation du moment comme générateur d'aléatoire
+    */
+    srand(time(NULL));
+    int max = 100, min = 1;
+    int playAgain = 1;
+
+    /*
+    First loop running until gamer is fed up with playing
+    Première boucle qui tourne jusqu'à ce que le joueur veuille s'arreter
+    */
+while (playAgain !=0)
+{
+
+    /*Menu*/
+        int menu = 0;
+printf("===menu===\n1.Easy: 1~100\n2.Medium: 1~1000\n3.Hard: 1~10000\n4.Custom: x~y\n");
+scanf("%d", &menu);
+switch (menu)
+{
+
+    case 1:
+        printf("Easy\n");
+        max = 100, min = 1;
+        game(max, min);
+        break;
+    case 2:
+        printf("Medium\n");
+        max = 1000, min = 1;
+        game(max, min);
+        break;
+    case 3:
+        printf("Hard\n");
+        max = 10000, min = 1;
+        game(max, min);
+        break;
+ /*   case 4:
+        printf("Custom\n");
+        //max = 100, min = 1;
+        printf("set min:")
+        scanf("%d", &min)
+        printf("set max:")
+        scanf("%d", &max)
+        game(max, min);
+        break;
+        */
+    default:
+        printf("try again\n");
+}
+            printf("Wanna play Again? (1: yes; 0:no)");
+            scanf("%d", &playAgain);
+            }
+printf("bye !!");
+   return 0;
 }
